f55bdca709a2537efc3120cbd9dbe53648e99ba3
Alam Hossain <alamhossain13100852@gmail.com>
2022-07-27 08:22:35 +0600

    double ==vs threeple ==

---
+++ double == vs three====.js
@@ -0,0 +1,20 @@
+const num1 = 1;
+const num2 = '1';
+//double ==
+// if (num1 == num2) {
+//     console.log('condition is true'); //answer true.
+//     //it doesn't check type
+// }
+// else {
+//     console.log('Condition is false');
+// }
+
+
+//===
+if (num1 === num2) {
+    console.log('condition is true'); //answer false.
+    //it check type
+}
+else {
+    console.log('Condition is false');
+}
