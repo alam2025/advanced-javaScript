441d8748dd29b5af1eb36ea210b6bad21f5bcf8a
Alam Hossain <alamhossain13100852@gmail.com>
2022-07-27 08:43:35 +0600

    map-filter

---
+++ map-filter.js
@@ -0,0 +1,19 @@
+const numbers = [3,4,5,6,7,8];
+// const output=[];
+// for(i=0 ;i<numbers.length;i++){
+//     const element = numbers[i];
+//     const power = element *element;
+//     output.push(power);
+// }
+// console.log(output)
+
+// const squre = numbers.map(x=>x*x);
+
+// console.log(squre)
+
+// const find = numbers.filter(x=>x%2==0);
+
+// console.log(find)
+
+const find = numbers.find(x=>x==4);
+console.log(find)
